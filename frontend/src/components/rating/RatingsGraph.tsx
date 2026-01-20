57de4fcbcd6b6d2429dab1ca43c2899cfad8c167
Temi <limiajayi@gmail.com>
2026-01-20 23:12:17 +0000

    Starting the D3 graph by adding error boundaries.

--- frontend/src/components/rating/RatingsGraph.tsx
+++ frontend/src/components/rating/RatingsGraph.tsx
@@ -1,15 +1,38 @@
-import type { User } from "../../types/user.types"
+import type { User } from "../../types/user.types";
+import { ErrorBoundary } from "react-error-boundary";
+// import * as d3 from 'd3';
+
+// type Quadrant = {
+//     x: number;
+//     y: number;
+//     width: number;
+//     height: number;
+//     label: string;
+//     color: string;
+// }
 
 type RatingsGraphProps = {
     user: User;
 }
 
+const ErrorFallback = () => {
+    return (
+        <div>
+            <h3>
+                Error detected!
+            </h3>
+        </div>
+    )
+}
+
 // where the d3 mess happens
 const RatingsGraph = ({ user }: RatingsGraphProps) => {
     return (
-        <div>
-            Yo I'm the future graph for {user?.username}
-        </div>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <div>
+                Yo I'm the future graph for {user?.username}
+            </div>
+        </ErrorBoundary>
     )
 }
 
